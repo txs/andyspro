fddeeb75baf9595dd185f276da5a1d3a0bc5ebef
txs <txshon@gmail.com>
2021-09-11 07:12:40 +0800

    init for setup

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '58e2b8cd439a47ff9520825068bd58af',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Andy\'s Pro',
+  domain: 'andys.pro',
+  author: 'Andy Tseng',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Andy\'s Pro',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: 'txshon',
+  github: 'txs',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
@@ -47,5 +47,13 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null
+  // pageUrlOverrides: null
+  pageUrlOverrides: {
+    '/tw': 'd7ee3960142449a3bcf03f9ddb108645',
+    '/blog': '41f2f0619fd441b8932b8ab5944fa466'
+
+  }
+
+
+
 }
