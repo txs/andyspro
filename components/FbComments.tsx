acd18ba779cdee8a72a433ab69c4ec8bfb4853d5
txs <txshon@gmail.com>
2022-01-27 04:55:01 +0800

    fix typescript type define

--- components/FbComments.tsx
+++ components/FbComments.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { FacebookProvider, Comments } from 'react-facebook';
 
-export default class Example extends Component {
+interface IRecipeProps {
+    href?: string;
+}
+
+interface IRecipeState {
+}
+
+export default class Example extends Component<IRecipeProps, IRecipeState> {
     render() {
         return (
             <FacebookProvider appId={process.env.NEXT_PUBLIC_FB_APPID}>
